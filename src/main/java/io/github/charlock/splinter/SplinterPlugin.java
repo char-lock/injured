7a4ca55321a4a0c4d20357dbf8cbdb10f8a2a596
C. Lockett <103963818+char-lock@users.noreply.github.com>
2022-10-26 05:20:57 -0400

    Fix speed issue with cripple injury.
    CHANGELOG
    
    * Added commands /injure, /uninjure, and /resetspeed
    * Added a check for double stacking healing and gaining cripple
    * Added a listener every time a player begins to move for the current speed and
      adjust speed according to injury.
    * Added a check for fall distance to be greater than 6 before processing cripple for
      fall damage.

---
+++ src/main/java/io/github/charlock/splinter/SplinterPlugin.java
@@ -0,0 +1,11 @@
+package io.github.charlock.splinter;
+
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.event.player.PlayerToggleSprintEvent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class SplinterPlugin extends JavaPlugin {
+    
+}
